1b18058a0812607fc8b57dcc21911c3d8b3aeec6
Leia-Luke <april.may.u041737@gmail.com>
2023-08-13 10:42:26 +0900

    hello

--- code.R
+++ code.R
@@ -5,5 +5,6 @@ data <- read.csv("owid-co2-data.csv")
 #データを取り出す
 selected_data <- data |> 
   select(country, year, gdp, population, co2_per_capita) |>
-  mutate()
-  
+  mutate(gdp_per_capita = gdp / population) |>
+  filter(country == "Japan") |>
+  na.rm = TRUE
