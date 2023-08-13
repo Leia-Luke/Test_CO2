4ff91dba998e5072d87b2f3ced94ccfa78cc4a75
shojiosamu <shfth_0216@yahoo.co.jp>
2023-08-13 10:54:25 +0900

    filter

--- code.R
+++ code.R
@@ -3,8 +3,10 @@ library(tidyverse)
 #データの読み込み
 data <- read.csv("owid-co2-data.csv")
 #データを取り出す
-selected_data <- data |> 
+selected_data<- data |> 
   select(country, year, gdp, population, co2_per_capita) |>
   mutate(gdp_per_capita = gdp / population) |>
-  filter(country == "Japan") |>
-  na.rm = TRUE
+  filter(country == "Japan", year %in% c(1850 : 2015))
+
+ggplot()
+  
