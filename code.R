169c94350b9a8164d8df38a0bb3884e22ab23c23
Leia-Luke <april.may.u041737@gmail.com>
2023-08-13 11:30:27 +0900

    graph with log and without log

--- code.R
+++ code.R
@@ -7,8 +7,15 @@ selected_data<- data |>
   select(country, year, gdp, population, co2_per_capita) |>
   mutate(gdp_per_capita = gdp / population) |>
   filter(country == "Japan", year %in% c(1850 : 2015))
-#横軸GDP/capita 縦軸CO2/capitaの折れ線プロット
+#横軸GDP/capita 縦軸CO2/capitaの折れ線プロット -log付
 ggplot(
-  data = selected_data, aes(x=log10(gdp_per_capita), y=co2_per_capita))+
+  data = selected_data, aes(x=log(gdp_per_capita), y=co2_per_capita))+
   geom_line() +
-  geom_point() 
+  geom_point() +
+  labs(x = "GDP/Capita", y = "CO2/Capita")
+#横軸GDP/capita 縦軸CO2/capitaの折れ線プロット -log無
+ggplot(
+  data = selected_data, aes(x=gdp_per_capita/1000, y=co2_per_capita))+
+  geom_line() +
+  geom_point() +
+  labs(x = "GDP/Capita", y = "CO2/Capita")
