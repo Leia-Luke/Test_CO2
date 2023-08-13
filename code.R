5ee7bad2696bcb34cd1251a74fab347dd5af93f6
drop1212 <satoshi144144@gmail.com>
2023-08-13 10:30:16 +0900

    code revised

--- code.R
+++ code.R
@@ -3,3 +3,7 @@ library(tidyverse)
 #データの読み込み
 data <- read.csv("owid-co2-data.csv")
 #データを取り出す
+selected_data <- data |> 
+  select(country, year, gdp, population, co2_per_capita) |>
+  mutate()
+  
